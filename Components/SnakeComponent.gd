3b6a90a6c6bd7292f3898f0566fb8e22fe7fa409
pringle farceur <63532428+wooshdude@users.noreply.github.com>
2023-11-08 13:57:45 -0800

    added comments

--- Components/SnakeComponent.gd
+++ Components/SnakeComponent.gd
@@ -1,30 +1,33 @@
 extends Node
 class_name SnakeComponent
+#  Controlls the visual body of the snake
+#  Does not influence movement
 
 @export var player: CharacterBody2D
 @export var texture: Texture
 @export var segments: int
 @export_range(0, 1, 0.1) var spacing: float
-@export var taper: Curve
-@export var curve: Curve
-@export var magnitude: float
+@export var taper: Curve  # Scales the body along the curve
+@export var curve: Curve  # Skews the body along the curve
+@export var magnitude: float  # How much the curve influences the skew
 
-var bodies: Array[Sprite2D]
-var points: Array[Vector2]
+var bodies: Array[Sprite2D]		# Stores individual segment objects
+var points: Array[Vector2]		# Saves previous positions
 var line = Line2D.new()
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	# Creates the line used for the body
 	line.width_curve = taper
 	line.joint_mode = Line2D.LINE_JOINT_ROUND
 	line.begin_cap_mode = Line2D.LINE_CAP_ROUND
 	line.end_cap_mode = Line2D.LINE_CAP_ROUND
 	self.add_child(line)
 	
+	# Adds segment objects
 	for i in range(segments):
 		var new_segment = Sprite2D.new()
-		#new_segment.texture = texture
 		new_segment.top_level = true
 		new_segment.global_position = player.global_position
 		bodies.append(new_segment)
@@ -32,22 +35,22 @@ func _ready():
 		self.add_child(new_segment)
 		line.add_point(Vector2.ZERO)
 	
+	# Fills the points array
 	for i in range(segments * spacing):
 		points.push_front(player.global_position)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	#if player.global_position != points[0]:
 	points.push_front(player.global_position)
-	#print(points[0])
 	
+	# Manages points array overflow
 	if points.size() > segments * spacing:
 		points.remove_at(points.size()-1)
 	
+	# Sets segment object positions
 	for i in range(segments):
-		bodies[i].global_position = points[i * (spacing*bodies[i].scale.x)]
-		bodies[i].position.x += curve.sample_baked(float(i)/float(segments)) * magnitude
+		bodies[i].global_position = points[i * (spacing*bodies[i].scale.x)]  # Sets global position, offset by spacing
+		bodies[i].position.x += curve.sample_baked(float(i)/float(segments)) * magnitude  # Skews segments along x axis
 		line.set_point_position(i,bodies[i].global_position)
-		#bodies[i].position.y += curve.sample_baked(float(i)/float(segments)) * magnitude
 	
--- Components/SnakeJump.gd
+++ Components/SnakeJump.gd
@@ -2,23 +2,22 @@ extends Node
 class_name SnakeJumpComponent
 
 @export var player: CharacterBody2D
-@export_range(1,10, 0.1) var strength: float
+@export_range(1,10, 0.1) var strength: float  # Jump strength
 
 
 func _physics_process(delta):
 	if not player.is_on_floor():
-		player.velocity.y += 30
+		player.velocity.y += 30  # Gravity
 	else:
-		player.velocity.x = lerp(player.velocity.x, 0.0, 0.3)
+		player.velocity.x = lerp(player.velocity.x, 0.0, 0.3)  # Floor friction
 	
 	if Input.is_action_just_pressed("l_click") and player.is_on_floor():
 		var direction = player.global_position.direction_to(player.get_global_mouse_position())
 		var distance = player.global_position.distance_to(player.get_global_mouse_position())
 		distance = clamp(distance, 10, 60)
 		
-		player.velocity = direction * strength * 60
+		player.velocity = direction * strength * 60  # Launches snake towards direction of cursor
 	elif Input.is_action_just_released("l_click") and not player.is_on_floor() and player.velocity.y < 0:
-		player.velocity.y = lerp(player.velocity.y, 0.0, 0.5)
-		#player.velocity.x = lerp(player.velocity.y, 0.0, 0.5)
+		player.velocity.y = lerp(player.velocity.y, 0.0, 0.5)  # Controls how suddenly the snake falls after mouse is released
 		
 	player.move_and_slide()
