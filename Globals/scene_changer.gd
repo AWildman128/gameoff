b736127e87745bddfa0df9edb99e83c11a018ad3
pringle farceur <63532428+wooshdude@users.noreply.github.com>
2023-11-24 01:41:55 -0800

    level select screen

--- Globals/scene_changer.gd
+++ Globals/scene_changer.gd
@@ -1,4 +1,17 @@
 extends Node
 
+@onready var transition = ColorRect.new()
+@onready var tween = self.get_tree().create_tween()
+
+
+func _ready():
+	transition.color = Color(0,0,0,0)
+	transition.set_anchors_preset(Control.PRESET_FULL_RECT)
+	self.add_child(transition)
+	
+
 func change_scene(target):
+	tween.tween_property(transition, "color", Color(0,0,0,255), 1)
+	await tween.finished
 	get_tree().change_scene_to_packed(target)
+	tween.tween_property(transition, "color", Color(0,0,0,0), 1)
