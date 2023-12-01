d24b834029174e43bd08eadfc7e5284e8f0cdcfe
pringle farceur <63532428+wooshdude@users.noreply.github.com>
2023-11-30 21:28:51 -0800

    hi-res main menu

--- Worlds/main_menu.gd
+++ Worlds/main_menu.gd
@@ -4,22 +4,32 @@ extends Control
 @onready var tab_container = $TabContainer
 
 func _ready():
+	get_tree().root.content_scale_mode = 1
+	$TabContainer.current_tab = 0
 	$TabContainer/Menu/MarginContainer/VBoxContainer/Start.grab_focus()
 	MusicManager.play_song(MusicManager.MAIN_MENU)
 
+
 func _on_start_pressed():
-	LevelManager.change_scene(world)
+	LevelManager.change_scene(preload("res://Worlds/level_select.tscn"))
 
 
 func _on_settings_pressed():
 	tab_container.current_tab = 1
 
+
 func _process(delta):
 	if Input.is_action_just_pressed("pause"):
 		tab_container.current_tab = 0
 
+
 func _on_quit_pressed():
 	get_tree().quit()
 
+
 func focus():
 		$TabContainer/Menu/MarginContainer/VBoxContainer/Start.grab_focus()
+
+
+func _exit_tree():
+	get_tree().root.content_scale_mode = 2
